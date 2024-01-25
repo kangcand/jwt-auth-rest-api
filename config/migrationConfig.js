bc21c328e39ffb7331ea60f3344e0689d37d0827
Candra Herdiansyah <candraherdiansyah14@gmail.com>
2024-01-25 22:24:59 +0700

    vercel

--- config/migrationConfig.js
+++ config/migrationConfig.js
@@ -1,4 +1,20 @@
-const config = require('./index');
-const cfg = {};
-cfg[conf.environment] = config.sequelize;
-module.exports = cfg;
+require("dotenv").config()
+
+module.exports = {
+  development: {
+    dialect: 'mysql',
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_HOST),
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  },
+  production: {
+    dialect: 'mysql',
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_HOST),
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  },
+};

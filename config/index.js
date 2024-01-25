5fb80226497de40c76bef5ec42b12ec2d67fae7f
Candra Herdiansyah <candraherdiansyah14@gmail.com>
2024-01-25 22:25:44 +0700

    vercel

--- config/index.js
+++ config/index.js
@@ -1,19 +1,19 @@
-const conf = {};
-require("dotenv").config()
+// const conf = {};
+// require("dotenv").config()
 
-conf.environment = 'development';
-conf.sequelize = {};
-conf.sequelize.username = process.env.DB_USERNAME;
-conf.sequelize.password = process.env.DB_PASSWORD;
-conf.sequelize.database = process.env.DB_NAME;
-conf.sequelize.host = process.env.DB_HOST;
-conf.sequelize.dialect = 'mysql';
-conf.sequelize.port = process.env.DB_PORT;
-conf.sequelize.define = {
-    charset: 'utf8mb4',
-    dialectOptions: {
-        collate: 'utf8mb4_unicode_ci'
-    }
-}
-conf.ROUND_SALT = 8;
-module.exports = conf;
+// conf.environment = 'development';
+// conf.sequelize = {};
+// conf.sequelize.username = process.env.DB_USERNAME;
+// conf.sequelize.password = process.env.DB_PASSWORD;
+// conf.sequelize.database = process.env.DB_NAME;
+// conf.sequelize.host = process.env.DB_HOST;
+// conf.sequelize.dialect = 'mysql';
+// conf.sequelize.port = process.env.DB_PORT;
+// conf.sequelize.define = {
+//     charset: 'utf8mb4',
+//     dialectOptions: {
+//         collate: 'utf8mb4_unicode_ci'
+//     }
+// }
+// conf.ROUND_SALT = 8;
+// module.exports = conf;

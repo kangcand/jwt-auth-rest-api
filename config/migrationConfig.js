a2622c11d9fe305d6233f41c620517c5eb08bd42
Candra Herdiansyah <candraherdiansyah14@gmail.com>
2024-01-25 22:42:52 +0700

    vercel

--- config/migrationConfig.js
+++ config/migrationConfig.js
@@ -1,10 +1,10 @@
-require("dotenv").config()
+require("dotenv").config();
 
 module.exports = {
   development: {
     dialect: 'mysql',
     host: process.env.DB_HOST,
-    port: Number(process.env.DB_HOST),
+    port: Number(process.env.DB_PORT), // Perbaiki bagian ini
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
@@ -12,10 +12,9 @@ module.exports = {
   production: {
     dialect: 'mysql',
     host: process.env.DB_HOST,
-    port: Number(process.env.DB_HOST),
+    port: Number(process.env.DB_PORT), // Dan juga perbaiki bagian ini
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
   },
 };
-// 
